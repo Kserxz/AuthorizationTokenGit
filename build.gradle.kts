d4f82b299b3e22cb55f30f21bb5718b40adcebc9
Kserxz <9streetdance1991@gmail.com>
2024-07-18 17:12:49 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.android.library) apply false
+}
+buildscript {
+    extra.apply{
+        set("compose_version", "1.0.3")
+    }
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath ("com.android.tools.build:gradle:7.0.2")
+        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
+
+    }
+}
